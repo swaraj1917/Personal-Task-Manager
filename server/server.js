2debb8d97e422ddfd81980a729da482f929de2c5
swaraj1917 <darkdevil45067@gmail.com>
2026-06-09 09:09:04 +0530

    Fix Render port configuration

--- server/server.js
+++ server/server.js
@@ -159,6 +159,8 @@ app.put("/tasks/reorder", (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
